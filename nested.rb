d044cd8565111a53adf519123c894fd6c27578bf
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-26 01:55:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,6 +57,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+		 						array = programmer_hash[:dennis_ritchie][:languages]
+		 						array.join
 end
 
 def adding_matz
@@ -82,6 +85,8 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
 
 end
 
@@ -103,8 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
-
+		 							programmer_hash[:alan_kay][:known_for] = alans_new_info
+									programmer_hash
 end
 
 def adding_to_dennis
@@ -123,5 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 
+		 						programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+								programmer_hash
 end
